50ec2d534837a7403729ec4ee4572893eae275cc
MaryGoroshko <marinka.izvekova@gmail.com>
2020-12-17 23:56:31 +0200

    Created new void print

--- src/homework18/Main.java
+++ src/homework18/Main.java
@@ -2,6 +2,10 @@ package homework18;
 
 public class Main {
     public static void main(String[] args) {
+        new Main().print();
+    }
 
+    void print() {
+        System.out.println("Hi");
     }
 }
