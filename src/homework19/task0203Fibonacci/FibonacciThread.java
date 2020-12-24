f486aae957e349ed649581199c245f8edfea210a
MaryGoroshko <marinka.izvekova@gmail.com>
2020-12-24 20:20:17 +0200

    Fixed problem with big numbers.
    * used BigInteger instead of int type.

--- src/homework19/task0203Fibonacci/FibonacciThread.java
+++ src/homework19/task0203Fibonacci/FibonacciThread.java
@@ -1,10 +1,10 @@
 package homework19.task0203Fibonacci;
 
+import java.math.BigInteger;
+
 public class FibonacciThread extends Thread {
 
     private int number;
-    private int num1 = 0;
-    private int num2 = 1;
 
     public FibonacciThread(int number) {
         this.number = number;
@@ -14,26 +14,19 @@ public class FibonacciThread extends Thread {
     public void run() {
         while (!this.isInterrupted()) {
             System.out.println("Start of calculation Fibonacci " + number);
-            if (number == 0 || number == 1) {
-                System.out.println(number);
+            if (number <= 1) System.out.println(BigInteger.valueOf(number));
+
+            BigInteger previous = BigInteger.ZERO, next = BigInteger.ONE, sum;
+
+            for (int i = 2; i <= number; i++) {
+
+                sum = previous;
+                previous = next;
+                next = sum.add(previous);
+                System.out.println("Sum= " + sum + " + " + previous);
             }
 
-            int fibonacciSum = 0;
-            for (int i = 0; i < number; i++) {
-                fibonacciSum = num1 + num2;
-                System.out.println("Sum= " + num1 + " + " + num2);
-                num1 = num2;
-                num2 = fibonacciSum;
-            }
-            System.out.println(fibonacciSum);
+            System.out.println("Result: " + next);
         }
-    }
-
-    public int getNum1() {
-        return num1;
-    }
-
-    public int getNum2() {
-        return num2;
     }
 }
