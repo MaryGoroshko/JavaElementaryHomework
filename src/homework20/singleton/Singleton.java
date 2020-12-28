9d991a62d157ecc1a9989b6ef15b3346b88f30c8
MaryGoroshko <marinka.izvekova@gmail.com>
2020-12-29 00:34:49 +0200

    Created Singleton with lazy initialization.

--- src/homework20/singleton/Singleton.java
+++ src/homework20/singleton/Singleton.java
@@ -1,14 +1,11 @@
 package homework20.singleton;
 
 public class Singleton {
+
     private static Singleton instance;
     private static int counterInt;
 
     public Singleton() {
-    }
-
-    public void counter() {
-        System.out.println("Int counter: " + counterInt);
     }
 
     public static Singleton getInstance() {
