1de3945ec513737cabe22c3e9f0ebf9e7736eeaf
MaryGoroshko <marinka.izvekova@gmail.com>
2020-12-29 20:35:38 +0200

    Created HelloWorld class.

---
+++ src/homework20/HelloWorld.java
@@ -0,0 +1,16 @@
+package homework20;
+
+/**
+ * Create a simple java program with 2 methods. One method prints “Hello” and the other method prints “World”.
+ */
+
+public class HelloWorld {
+
+    public void printHello(){
+        System.out.print("Hello ");
+    }
+
+    public void printWorld(){
+        System.out.print("World ");
+    }
+}
